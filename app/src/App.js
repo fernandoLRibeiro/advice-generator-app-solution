52ee731f3d6dcfe02c8268aae3459ea9d5cf2048
Fernando L. Ribeiro <fernandoribeiro@protonmail.com>
2022-07-22 11:47:15 -0300

    Set basic global CSS variables

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import styles from "./styles/App.module.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className={styles.App}></div>;
 }
 
 export default App;
